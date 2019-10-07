df7cb564a3f3726c40d4fd5ba89781ff3b973ab3
Lees-Oz <lev.guzeev@gmail.com>
2019-10-07 21:57:57 +0200

    3rd one for basic challenge

--- src/main/scala/WordFunnel.scala
+++ src/main/scala/WordFunnel.scala
@@ -17,16 +17,23 @@ object WordFunnel extends App {
           .toString())
     .toSet
 
-  def funnel1(w: String, acc: Set[String]): Set[String] =
-    if (words.contains(w)) minusLetter(w).toList.map(ww => funnel1(ww, acc + w)).max
+  def funnel0(w: String, acc: Int): Int =
+    if (words.contains(w)) minusLetter(w).map(ww => funnel0(ww, acc + 1)).max
     else acc
 
-  println(funnel1(word, Set()))
+  println(funnel0(word, 0)) // 4
+
+
+  def funnel1(w: String, acc: Set[String]): Set[String] =
+    if (words.contains(w)) minusLetter(w).map(ww => funnel1(ww, acc + w)).max
+    else acc
+
+  println(funnel1(word, Set())) // Set(gnash, gash, ash, sh)
 
 
   def funnel2(w: String, acc: Set[String]): Set[Set[String]] =
     if(words.contains(w)) minusLetter(w).flatMap(ww => funnel2(ww, acc + w))
     else Set(acc)
 
-  println(funnel2(word, Set()))
+  println(funnel2(word, Set()).toList.sorted.reverse) // List(Set(gnash, gash, ash, ah), Set(gnash, gash, ash, sh), Set(gnash, gash, gas, as), Set(gnash, gash, ash, as), Set(gnash, gash, gas), Set(gnash, gash), Set(gnash))
 }
