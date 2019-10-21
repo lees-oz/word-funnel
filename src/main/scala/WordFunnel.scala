17d0f613277fb276e85e056e94d16e86137ea92e
Lev Guzeev <lev.guzeev@gmail.com>
2019-10-21 13:57:37 +0200

    minus letter done functionally; isSubwordOf function

--- src/main/scala/WordFunnel.scala
+++ src/main/scala/WordFunnel.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object WordFunnel extends App {
   import scala.io.Source
   val html = Source.fromURL("https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt")
@@ -9,31 +11,55 @@ object WordFunnel extends App {
 
   def isWord: String => Boolean = words.contains
 
-  def minusLetter(w: String): Set[String] =
-    (0 until w.length)
-      .map(i =>
-        new StringBuilder(w)
-          .deleteCharAt(i)
-          .toString())
-    .toSet
+  def minLetter(w: String): Set[String] = {
+    @tailrec
+    def iterate(i: Int, left: String, right: String): String = {
+      if(right.isEmpty) left
+      else if(i == 0) left + right.tail
+      else iterate(i - 1, left + right.head, right.tail)
+    }
 
-  def funnel0(w: String, acc: Int): Int =
-    if (words.contains(w)) minusLetter(w).map(ww => funnel0(ww, acc + 1)).max
+    (0 until w.length).map(i => iterate(i, "", w)).toSet
+  }
+
+  println(minLetter("abro")) // Set(bro, aro, abo, abr)
+
+  // Longest funnel length
+  def funnel0(w: String, acc: Int = 0): Int =
+    if (words.contains(w)) minLetter(w).map(ww => funnel0(ww, acc + 1)).max
     else acc
 
-  println(funnel0(word, 0)) // 4
+  println(funnel0(word)) // 4
 
-
-  def funnel1(w: String, acc: Set[String]): Set[String] =
-    if (words.contains(w)) minusLetter(w).map(ww => funnel1(ww, acc + w)).max
+  // Longest funnel
+  def funnel1(w: String, acc: Set[String] = Set()): Set[String] =
+    if (words.contains(w)) minLetter(w).map(ww => funnel1(ww, acc + w)).max
     else acc
 
-  println(funnel1(word, Set())) // Set(gnash, gash, ash, sh)
+  println(funnel1(word)) // Set(gnash, gash, ash, sh)
 
 
-  def funnel2(w: String, acc: Set[String]): Set[Set[String]] =
-    if(words.contains(w)) minusLetter(w).flatMap(ww => funnel2(ww, acc + w))
+  // All funnels
+  def funnel2(w: String, acc: Set[String] = Set()): Set[Set[String]] =
+    if(words.contains(w)) minLetter(w).flatMap(ww => funnel2(ww, acc + w))
     else Set(acc)
 
-  println(funnel2(word, Set()).toList.sorted.reverse) // List(Set(gnash, gash, ash, ah), Set(gnash, gash, ash, sh), Set(gnash, gash, gas, as), Set(gnash, gash, ash, as), Set(gnash, gash, gas), Set(gnash, gash), Set(gnash))
+  println(funnel2(word).toList.sorted.reverse) // List(Set(gnash, gash, ash, ah), Set(gnash, gash, ash, sh), Set(gnash, gash, gas, as), Set(gnash, gash, ash, as), Set(gnash, gash, gas), Set(gnash, gash), Set(gnash))
+
+  println(funnel2("princesses")) // HashSet(HashSet(pi, princess, princesse, princes, princesses, pis, prices, pies, pries), ...
+
+
+
+  // Optional 2 WIP
+  @tailrec
+  def isSubwordOf(s: String, w: String): Boolean = {
+    if(w.isEmpty) s.isEmpty
+    else if(s.isEmpty) true
+    else if(w.length < s.length) false
+    else if(s.head == w.head) isSubwordOf(s.tail, w.tail)
+    else isSubwordOf(s, w.tail)
+  }
+
+  println(isSubwordOf("aba", "abbadon")) // true
+
 }
